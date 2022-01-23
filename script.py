3f063353f4d66031d50ba3d5c4f5ba79472c8fd7
Paulo Henrique <paulo.junior.ph@gmail.com>
2022-01-23 19:22:11 -0300

    Adicionado função para programar script

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import os
 from tkinter import *
 from tkinter import messagebox
+import schedule
+import time
 
 class Func():
     def add(self):
@@ -19,18 +21,24 @@ class Func():
             cont +=1
             self.lb_3["text"] = "Total de: {}".format(cont)
         self.lb_2["text"] = "{}".format (self.lista)
+    def programar(self):
+        dados = int(self.entrada_2.get())
+        schedule.every(dados).minutes.do(self.rodar_script)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
 
 class Aplicativo(Func):
     def __init__(self):
         root = Tk()
-        root.geometry("260x170+547+250")
+        root.geometry("270x260+547+250")
         root.title("Gerar script")
         self.lista = []
         self.root = root
         self.widgets()
         mainloop()
     def widgets(self):
-        self.lb_1 = Label(self.root, text="Informe o programa ou site para add ao script").pack()
+        Label(self.root, text="Informe o programa ou site para add ao script").pack()
         self.entrada_1 = Entry(self.root)
         self.entrada_1.pack()
         self.btn_add_script = Button(self.root, text="Add ao script", command=self.add).pack()
@@ -39,6 +47,9 @@ class Aplicativo(Func):
         self.lb_2.pack()
         self.lb_3 = Label(self.root, text="")
         self.lb_3.pack()
-        self.btn_rodar_script = Button(self.root, text="Rodar script", command=self.rodar_script).pack()
-        
+        self.btn_rodar_script = Button(self.root, text="Rodar script agora", command=self.rodar_script).pack()
+        Label(self.root, text="Se preferir você pode informar abaixo\no intervalo em minutos para rodar o script").pack()
+        self.entrada_2 = Entry(self.root)
+        self.entrada_2.pack()
+        self.btn_programar = Button(self.root, text="Programar", command=self.programar).pack()
 Aplicativo()
